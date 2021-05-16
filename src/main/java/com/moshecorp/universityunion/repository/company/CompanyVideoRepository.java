d5a07e7231ab3fa09eee9b702f49e71a912000e7
Pawel Szepelewicz <pashashepel1997@outlook.com>
2021-05-16 21:47:41 +0300

    @Param

--- src/main/java/com/moshecorp/universityunion/repository/company/CompanyVideoRepository.java
+++ src/main/java/com/moshecorp/universityunion/repository/company/CompanyVideoRepository.java
@@ -2,10 +2,14 @@ package com.moshecorp.universityunion.repository.company;
 
 import com.moshecorp.universityunion.model.company.CompanyVideo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
 public interface CompanyVideoRepository extends JpaRepository<CompanyVideo, Long> {
+
+    @Query(value = "select videoUrl from CompanyVideo where companyId = :companyId")
+    String getVideoUrlByCompanyId(@Param("companyId") Long companyId);
 
 }
--- src/main/java/com/moshecorp/universityunion/repository/user/UserPhotoRepository.java
+++ src/main/java/com/moshecorp/universityunion/repository/user/UserPhotoRepository.java
@@ -3,6 +3,7 @@ package com.moshecorp.universityunion.repository.user;
 import com.moshecorp.universityunion.model.user.UserPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,5 @@ import java.util.List;
 public interface UserPhotoRepository extends JpaRepository<UserPhoto, Long> {
 
     @Query(value = "select photoUrl from UserPhoto where userId = :userId")
-    List<String> getPhotoUslListByUserId(Long userId);            //utw
+    List<String> getPhotoUslListByUserId(@Param("userId") Long userId);            //utw
 }
