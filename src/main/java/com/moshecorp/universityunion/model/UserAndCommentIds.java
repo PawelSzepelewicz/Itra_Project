ea1cb4389b623d059c96dcced6f3d8597ce9093e
Pawel Szepelewicz <pashashepel1997@outlook.com>
2021-05-17 17:40:05 +0300

    @NoArgsConstructor

--- src/main/java/com/moshecorp/universityunion/model/UserAndCommentIds.java
+++ src/main/java/com/moshecorp/universityunion/model/UserAndCommentIds.java
@@ -2,9 +2,11 @@ package com.moshecorp.universityunion.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserAndCommentIds {
 
     Long userId;
