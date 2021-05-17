13e484f8373581aa751bd1ed2e538633f39942fc
Pawel Szepelewicz <pashashepel1997@outlook.com>
2021-05-17 12:12:40 +0300

    changeRole

--- src/main/java/com/moshecorp/universityunion/service/user/impl/UiSettingsServiceImpl.java
+++ src/main/java/com/moshecorp/universityunion/service/user/impl/UiSettingsServiceImpl.java
@@ -2,7 +2,9 @@ package com.moshecorp.universityunion.service.user.impl;
 
 import com.moshecorp.universityunion.model.UserRole;
 import com.moshecorp.universityunion.model.user.UiSettings;
+import com.moshecorp.universityunion.model.user.User;
 import com.moshecorp.universityunion.repository.user.UiSettingsRepository;
+import com.moshecorp.universityunion.repository.user.UserRepository;
 import com.moshecorp.universityunion.service.user.UiSettingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ public class UiSettingsServiceImpl implements UiSettingsService {
 
     @Autowired
     UiSettingsRepository uiSettingsRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UiSettings changeUiSettings(UiSettings uiSettings) {   //utw
@@ -23,5 +27,8 @@ public class UiSettingsServiceImpl implements UiSettingsService {
         UiSettings uiSettings = uiSettingsRepository.getByUserId(userRole.getUserId());
         uiSettings.setRole(userRole.getRole());
         uiSettingsRepository.save(uiSettings);
+        User user = userRepository.getById(userRole.getUserId());
+        user.setRole(userRole.getRole());
+        userRepository.save(user);
     }
 }
